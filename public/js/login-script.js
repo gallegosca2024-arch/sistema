c4e70e3746f31e983d92a00af93049813fb3368e
gallegosca2024-arch <gallegosca2024@gmail.com>
2025-12-10 22:56:00 -0500

    versión 1 login y home

---
+++ public/js/login-script.js
@@ -0,0 +1,17 @@
+//
+// Archivo: public/js/login-script.js
+// Funcionalidad: Alternar la visibilidad de la contraseña
+//
+$(document).ready(function() {
+    $(".toggle-password").click(function() {
+
+      $(this).toggleClass("fa-eye fa-eye-slash");
+      var input = $($(this).attr("toggle"));
+      
+      if (input.attr("type") == "password") {
+        input.attr("type", "text");
+      } else {
+        input.attr("type", "password");
+      }
+    });
+});
---
+++ view/adminLogin/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>SISTEMA DE CERTIFICACIÓN :: login</title>
+
+    <link href="../../public/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="../../public/lib/Ionicons/css/ionicons.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../../public/css/bracket.css">
+
+</head>
+
+<body>
+
+    <div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
+
+        <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white rounded shadow-base">
+            <div class="signin-logo tx-center">
+                <img src="../../public/img/LOGO_KAIROSIG.png" alt="Logo KAIROSIG">
+            </div>
+            <div class="tx-center mg-b-60">Sistema de Certificación</div>
+
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Ingresa tu nombre de usuario">
+            </div>
+            <div class="form-group">
+                <div class="input-group">
+                    <input type="password" id="password-field" class="form-control" placeholder="Ingresa tu contraseña">
+                    <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                </div>
+                <a href="" class="tx-info tx-12 d-block mg-t-10">¿Olvidaste tu contraseña?</a>
+            </div>
+            <button type="submit" class="btn btn-info btn-block">Iniciar Sesión</button>
+        </div>
+    </div>
+    <script src="../../public/lib/jquery/jquery.js"></script>
+    <script src="../../public/lib/popper.js/popper.js"></script>
+    <script src="../../public/lib/bootstrap/bootstrap.js"></script>
+
+    <script src="../../public/js/login-script.js"></script>
+
+</body>
+
+</html>
